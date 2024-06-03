64449ade73ba5c3d2f41b93ee85e2fb1cda27424
ivy-lv11 <zhicunlv@gmail.com>
2024-06-03 16:04:45 +0800

    unify

--- libs/community/langchain_community/llms/ipex_llm.py
+++ libs/community/langchain_community/llms/ipex_llm.py
@@ -1,5 +1,5 @@
 import logging
-from typing import Any, List, Literal, Mapping, Optional
+from typing import Any, List, Mapping, Optional
 
 from langchain_core.callbacks import CallbackManagerForLLMRun
 from langchain_core.language_models.llms import LLM
@@ -46,7 +46,6 @@ class IpexLLM(LLM):
         tokenizer_id: Optional[str] = None,
         load_in_4bit: bool = True,
         load_in_low_bit: Optional[str] = None,
-        device_map: Literal["cpu", "xpu"] = "cpu",
         **kwargs: Any,
     ) -> LLM:
         """
@@ -76,7 +75,6 @@ class IpexLLM(LLM):
             low_bit_model=False,
             load_in_4bit=load_in_4bit,
             load_in_low_bit=load_in_low_bit,
-            device_map=device_map,
             model_kwargs=model_kwargs,
             kwargs=kwargs,
         )
@@ -88,7 +86,6 @@ class IpexLLM(LLM):
         model_kwargs: Optional[dict] = None,
         *,
         tokenizer_id: Optional[str] = None,
-        device_map: Literal["cpu", "xpu"] = "cpu",
         **kwargs: Any,
     ) -> LLM:
         """
@@ -112,7 +109,6 @@ class IpexLLM(LLM):
             low_bit_model=True,
             load_in_4bit=False,  # not used for low-bit model
             load_in_low_bit=None,  # not used for low-bit model
-            device_map=device_map,
             model_kwargs=model_kwargs,
             kwargs=kwargs,
         )
@@ -125,7 +121,6 @@ class IpexLLM(LLM):
         load_in_4bit: bool = False,
         load_in_low_bit: Optional[str] = None,
         low_bit_model: bool = False,
-        device_map: Literal["cpu", "xpu"] = "cpu",
         model_kwargs: Optional[dict] = None,
         kwargs: Optional[dict] = None,
     ) -> Any:
@@ -147,6 +142,16 @@ class IpexLLM(LLM):
         kwargs = kwargs or {}
 
         _tokenizer_id = tokenizer_id or model_id
+        # Set "cpu" as default device
+        if "device" not in model_kwargs:
+            model_kwargs["device"] = "cpu"
+
+        if model_kwargs["device"] not in ["cpu", "xpu"]:
+            raise ValueError(
+                "IpexLLMBgeEmbeddings currently only supports device to be "
+                f"'cpu' or 'xpu', but you have: {model_kwargs['device']}."
+            )
+        device = model_kwargs.pop("device")
 
         try:
             tokenizer = AutoTokenizer.from_pretrained(_tokenizer_id, **_model_kwargs)
@@ -194,14 +199,7 @@ class IpexLLM(LLM):
                 model_kwargs=_model_kwargs,
             )
 
-        # Set "cpu" as default device
-
-        if device_map not in ["cpu", "xpu"]:
-            raise ValueError(
-                "IpexLLM currently only supports device to be "
-                f"'cpu' or 'xpu', but you have: {device_map}."
-            )
-        model.to(device_map)
+        model.to(device)
 
         return cls(
             model_id=model_id,
@@ -252,7 +250,7 @@ class IpexLLM(LLM):
             from transformers import TextStreamer
 
             input_ids = self.tokenizer.encode(prompt, return_tensors="pt")
-            input_ids.to(self.model.device)
+            input_ids = input_ids.to(self.model.device)
             streamer = TextStreamer(
                 self.tokenizer, skip_prompt=True, skip_special_tokens=True
             )
@@ -279,7 +277,7 @@ class IpexLLM(LLM):
             return text
         else:
             input_ids = self.tokenizer.encode(prompt, return_tensors="pt")
-            input_ids.to(self.model.device)
+            input_ids = input_ids.to(self.model.device)
             if stop is not None:
                 from transformers.generation.stopping_criteria import (
                     StoppingCriteriaList,
